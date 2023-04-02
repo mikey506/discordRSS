bddb043a12c12c9daf8b7eb917c22f03c61d7ddd
Michael Landry <michaellandry3@hotmail.com>
2023-04-02 11:46:48 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,75 +1,68 @@
 import discord
 import feedparser
-import pickle
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Define the RSS feed URLs and the corresponding Discord channels
-feeds = {
-    "https://nbmediacoop.org/feed/": 123456789012345678,
-    "http://feeds.feedburner.com/TourismNewBrunswick": 234567890123456789,
-    # ... other feeds ...
-    "https://www.iheartradio.ca/feed/podcast-1/pure-country-weekends-1.9209392": 678901234567890123
-}
+# Define RSS feeds to monitor
+feeds = [
+    ("https://www.buzzfeed.com/world.xml", 123456789),  # Example
+    # Add more feeds here
+]
 
-# Load the stored feed information from a file, if it exists
+# Load or create pickle file for storing feed information
 try:
     with open("feeds.pickle", "rb") as f:
-        stored_feeds = pickle.load(f)
+        feed_info = pickle.load(f)
 except FileNotFoundError:
-    stored_feeds = {}
+    feed_info = {}
 
 # Create a Discord client
-client = discord.Client()
+client = discord.Client(intents=None)
 
-@app.route('/')
-def index():
-    feed_info = [(url, channel_id) for url, channel_id in feeds.items()]
-    return render_template('index.html', feeds=feed_info)
-
+# Discord event handlers
 @client.event
 async def on_ready():
-    print("Logged in as {0.user}.".format(client))
+    print(f"Logged in as {client.user.name} ({client.user.id})")
 
 @client.event
-async def on_error(event, *args, **kwargs):
-    print("Error:", args[0])
-
-@client.event
-async def on_disconnect():
-    print("Disconnected.")
+async def on_member_join(member):
+    # Send a welcome message to the member's default channel
+    channel = member.guild.default_channel
+    if channel:
+        await channel.send(f"Welcome to the server, {member.mention}!")
 
 @client.event
 async def on_message(message):
-    if message.content == "!listfeeds":
-        # Show a list of all the monitored feeds and their corresponding channels
-        feed_list = "\n".join([f"{url}: {channel_id}" for url, channel_id in feeds.items()])
-        await message.channel.send("Monitored feeds:\n" + feed_list)
+    if message.author == client.user:
+        return
 
-# Monitor the RSS feeds and post updates to Discord
-async def monitor_feeds():
-    await client.wait_until_ready()
+    # Check if message contains any of the feed URLs
+    for url, channel_id in feeds:
+        if url in message.content:
+            # Check if the feed has been posted recently
+            if url not in feed_info or time.time() - feed_info[url]["last_post_time"] > 86400:
+                # Fetch the latest feed entry
+                feed = feedparser.parse(url)
+                if feed.entries:
+                    latest_entry = feed.entries[0]
+                    # Post the entry to the specified channel
+                    channel = client.get_channel(channel_id)
+                    await channel.send(f"New post in {url}: {latest_entry.title}\n{latest_entry.link}")
+                    # Update the last post time for the feed
+                    feed_info[url] = {"last_post_time": time.time()}
+                    with open("feeds.pickle", "wb") as f:
+                        pickle.dump(feed_info, f)
+                else:
+                    await message.channel.send(f"No entries found for {url}")
+            else:
+                await message.channel.send(f"A post from {url} was already made today")
 
-    while not client.is_closed():
-        for url, channel_id in feeds.items():
-            # Check if the feed has been updated since the last time it was checked
-            last_checked = stored_feeds.get(url, None)
-            feed = feedparser.parse(url)
-            latest_entry = feed.entries[0]
-            if last_checked is None or latest_entry.published_parsed > last_checked:
-                # Post the update to the Discord channel
-                channel = client.get_channel(channel_id)
-                await channel.send(f"**{feed.feed.title}**\n{latest_entry.title}\n{latest_entry.link}")
+# Flask route for displaying monitored feeds
+@app.route("/")
+def index():
+    return render_template("index.html", feeds=feeds)
 
-                # Update the stored feed information
-                stored_feeds[url] = latest_entry.published_parsed
-                with open("feeds.pickle", "wb") as f:
-                    pickle.dump(stored_feeds, f)
-
-        # Wait for 10 minutes before checking the feeds again
-        await asyncio.sleep(600)
-
-# Start the Discord client and the feed monitor loop
-client.loop.create_task(monitor_feeds())
-client.run("<your_bot_token>")
+# Start the bot and Flask app
+client.run("YOUR_DISCORD_BOT_TOKEN")
+app.run(port=8080, debug=True)
